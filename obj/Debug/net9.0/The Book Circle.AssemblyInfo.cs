f12489488fca8936cdd0c98540ab0b92c5636a81
Mustafa Hassan <91998433+call911kid@users.noreply.github.com>
2025-10-16 01:29:50 +0300

    first commit

--- obj/Debug/net9.0/The Book Circle.AssemblyInfo.cs
+++ obj/Debug/net9.0/The Book Circle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The Book Circle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1f2f7e43d2921777451bcc39f489c916d93b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d244d8012fb4fb5f4235fd17c6be9dbbeca207a")]
 [assembly: System.Reflection.AssemblyProductAttribute("The Book Circle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The Book Circle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
